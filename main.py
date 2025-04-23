34f9042ff1b334d34c77c69b4fa9ebbabceb5f95
ts772 <tskinner347@gmail.com>
2025-04-23 09:20:47 -0400

    Added groups to main function

--- main.py
+++ main.py
@@ -6,6 +6,10 @@ from constants import *
 from player import * 
 from circleshape import *
 
+updatable = pygame.sprite.Group()
+drawable = pygame.sprite.Group()
+Player.containers = (updatable, drawable)
+
 def main():
     pygame.init
     print("Starting Asteroids!")
@@ -19,9 +23,11 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
+        updatable.update(dt)    
         screen.fill("black")
-        player.draw(screen)
-        player.update(dt)
+        for object in drawable:
+            object.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60)/1000
 
