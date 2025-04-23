d1a8c9bc76fc08926e0621b38e19aa0b924b7ab2
ts772 <tskinner347@gmail.com>
2025-04-23 08:58:56 -0400

    Updated main function to draw player

--- main.py
+++ main.py
@@ -3,6 +3,8 @@
 # throughout this file
 import pygame
 from constants import *
+from player import * 
+from circleshape import *
 
 def main():
     pygame.init
@@ -10,12 +12,17 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    while pygame.get_init:
+    clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+    dt = 0
+    while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-    screen.fill("black")
-    pygame.display.flip()
+        screen.fill("black")
+        player.draw(screen)
+        pygame.display.flip()
+        dt = clock.tick(60)/1000
 
 
 
